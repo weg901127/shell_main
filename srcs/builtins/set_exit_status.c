fee821ea39386f49e0588dc131854b9d1f797891
sehhong <shhong3317@gmail.com>
2021-12-15 16:17:42 +0900

    날리지마라 짱나니까

--- srcs/builtins/set_exit_status.c
+++ srcs/builtins/set_exit_status.c
@@ -6,14 +6,14 @@
 /*   By: sehhong <sehhong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 14:35:25 by sehhong           #+#    #+#             */
-/*   Updated: 2021/12/15 14:37:22 by sehhong          ###   ########.fr       */
+/*   Updated: 2021/12/15 16:07:56 by sehhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../micro_shell.h"
 
-//environ의 요소중 $0, $?를 정해진 위치에 두면 좋을 듯.
+//runtime_env의 요소중 $0, $?를 정해진 위치에 두면 좋을 듯.
 void	set_exit_status(t_storage *bag, int exit_status)
 {
-	getALElement(bag->environ->pElement, 1)->data = ft_itoa(exit_status);
+	getALElement(bag->runtime_env->pElement, 1)->data = ft_itoa(exit_status);
 }
