a582605317d480eed8b6ae9088880d79acc3d697
sehhong <shhong3317@gmail.com>
2021-12-15 21:29:11 +0900

    finalize

--- srcs/builtins/builtin_echo.c
+++ srcs/builtins/builtin_echo.c
@@ -6,16 +6,17 @@
 /*   By: sehhong <sehhong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 01:26:59 by sehhong           #+#    #+#             */
-/*   Updated: 2021/12/15 16:41:41 by sehhong          ###   ########.fr       */
+/*   Updated: 2021/12/15 16:56:18 by sehhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../micro_shell.h"
 
-bool	builtin_echo(char *str, int n_option)
+//TODO error가 일어날 경우가 언제가 있지? 있을 경우, exit code는?
+bool	builtin_echo(t_storage *bag, char *str, int n_option)
 {
 	ft_putstr_fd(str, 1);
 	if (!n_option)
 		write(1, "\n", 1);
-	//환경변수 ?를 바꾸는 함수
+	set_exit_status(bag, EXIT)
 }
