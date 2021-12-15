8d8991915b1f4c0aa321ccb02a18fe5b2024d473
gilee <gilee@student.42seoul.kr>
2021-12-15 16:43:49 +0900

    add input, parse_buf

--- srcs/parse/prase_master.c
+++ srcs/parse/prase_master.c
@@ -6,10 +6,25 @@
 /*   By: gilee <gilee@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 12:45:45 by gilee             #+#    #+#             */
-/*   Updated: 2021/12/15 12:46:40 by gilee            ###   ########.fr       */
+/*   Updated: 2021/12/15 16:42:44 by gilee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../micro_shell.h"
 
-bool parse_master()
+bool single_quote(t_storage *bag)
+{
+	bool	res;
+
+	res = false;
+	return (res);
+}
+
+bool parse_master(t_storage *bag)
+{
+	bool	res;
+
+	res = false;
+	
+	return (res);
+}
